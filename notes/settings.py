73c725b0be7a7fc3e47e77b5b8ef56c6cb9e6f52
alkaf <>
2020-08-26 11:29:05 +0300

    edit templates and add User login,logout, signup, profile

--- notes/settings.py
+++ notes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes_app',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+LOGIN_REDIRECT_URL = '/notes'
--- notes/urls.py
+++ notes/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notes/', include('notes_app.urls', namespace= 'notes')),
+    path('accounts/', include('accounts.urls', namespace= 'accounts')),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
